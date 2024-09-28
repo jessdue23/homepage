d76256ddb0475a7b3c7c5720ba69e6baa44714b2
Jessica <jduesing23@gmail.com>
2024-09-27 21:50:48 -0500

    Added carousel

--- site.js
+++ site.js
@@ -1,6 +1,8 @@
 
 //GET DIV FOR DISPLAYING WELCOME MESSAGE - Assignment 1
 const welcomeMessageElement = document.querySelector('#welcome');
+const btnNext = document.querySelector('#next')
+const btnPrev = document.querySelector('#prev')
 
 
 //GET DATE and CONDITIONAL VARIABLE LOGIC
@@ -28,3 +30,43 @@ else{
 
 ////////////ASSIGNMENT 2 - SECRET MESSAGE/////////////////////////
 localStorage.setItem("It's a secret to everybody.", "Google says this is a Zelda reference?");
+
+////carousel///////
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000)
+
+//next button event listener
+btnNext.addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+
+
+//previous button
+btnPrev.addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
